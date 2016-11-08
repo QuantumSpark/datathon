0055aa4ae2956f99b930a0047a10e2885fc53805
James Park <QuantumSpark@Jamess-MacBook-Pro.local>
2016-11-08 00:14:21 -0800

    minor changes

--- googleMap.py
+++ googleMap.py
@@ -9,32 +9,41 @@ GoogleMaps(app, key="AIzaSyCwc3ISug1xPFbSP7kL4f4xF_svNgAc2bc")
 def plotPointsOnMap():
     rawTrafficSignal = pd.read_csv('DataCSVFiles/trafficsignals.csv')
     trafficCameras = pd.read_csv('DataCSVFiles/monthlytrafficcameras.csv')
-
-    marker = [{} for _ in range(rawTrafficSignal.Latitude.size )]
+    avgLat = 0
+    avgLong = 0
+    markerTrafficSignal = [{} for _ in range(rawTrafficSignal.Latitude.size)]
     for i in range(int(rawTrafficSignal.Longitude.size)):
+        avgLat += rawTrafficSignal.Latitude[i]
+        avgLong += rawTrafficSignal.Longitude[i]
         dic = {
             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
             'lat': rawTrafficSignal.Latitude[i],
             'lng': rawTrafficSignal.Longitude[i],
-            'infobox': "<b>traffic signal</b>"+str(i)
+            'infobox': "<b>traffic signal</b>" + str(rawTrafficSignal.Latitude[i]) + "," + str(rawTrafficSignal.Longitude[i])
         };
-        marker[i] = dic
+        markerTrafficSignal[i] = dic
 
-    marker2 = [{} for _ in range(trafficCameras.LATITUDE.size)]
+    markerTrafficCamera = [{} for _ in range(trafficCameras.LATITUDE.size)]
     for i in range(int(trafficCameras.LATITUDE.size)):
+        avgLat += trafficCameras.LATITUDE[i]
+        avgLong += trafficCameras.LONGITUDE[i]
         dic = {
             'icon': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
             'lat': trafficCameras.LATITUDE[i],
             'lng': trafficCameras.LONGITUDE[i],
-            'infobox': "<b>traffic camera</b>" + str(i)
+            'infobox': "<b>traffic camera</b>" + str(trafficCameras.LATITUDE[i]) + "," + str(trafficCameras.LONGITUDE[i])
         };
-        marker2[i] = dic
-    markers = marker + marker2
+        markerTrafficCamera[i] = dic
+    markers = markerTrafficSignal + markerTrafficCamera
+    avgLat = avgLat/(trafficCameras.LATITUDE.size + rawTrafficSignal.Latitude.size)
+    avgLong = avgLong/(trafficCameras.LATITUDE.size + rawTrafficSignal.Latitude.size)
+    print(avgLat)
+    print(avgLong)
     sndmap = Map(
         identifier="sndmap",
-        zoom= 7,
-        lat=49.09401236,
-        lng=-122.8180491,
+        zoom= 11,
+        lat=avgLat,
+        lng=avgLong,
         markers=markers
     )
     return render_template('mapthing.html', sndmap=sndmap)
