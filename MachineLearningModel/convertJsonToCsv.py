ac55d3dd9c3725c5a2bf581a8f414b74b708a2e2
James Park <QuantumSpark@Jamess-MacBook-Pro.local>
2016-11-15 15:41:20 -0800

    Created model for both open and closed request

--- MachineLearningModel/convertJsonToCsv.py
+++ MachineLearningModel/convertJsonToCsv.py
@@ -1,15 +1,23 @@
 import json
 import csv
 
-with open ("datathon/surrey_api/data/requests_20160101_20161112_OPEN.json", "r") as myfile:
-    x=str(myfile.read().replace('\n', ''))
-x = json.loads(x)
-print(x)
-f = csv.writer(open("requets_20160101_20161112_OPEN.csv", "wb+"))
+inputJsonPathOpen =   "datathon/surrey_api/data/requests_20160101_20161112_OPEN.json"
+inputJsonPathClosed = "datathon/surrey_api/data/requests_20160101_20161112_CLOSED.json"
 
 
-f.writerow(["description", "agency_responsible"])
+outputCsvPathOpen =   "request_20160101_20161112_OPEN.csv"
+outputCsvPathClosed = "request_20160101_20161112_CLOSED.csv"
 
-for x in x:
-    f.writerow([x["description"],
-                x["agency_responsible"]])
+def convertJsonToCsv(inputJsonPath, outputCsvPath):
+    with open (inputJsonPath, "r") as myfile:
+        x=str(myfile.read().replace('\n', ''))
+    x = json.loads(x)
+
+    f = csv.writer(open(outputCsvPath, "wb+"))
+
+
+    f.writerow(["description", "agency_responsible"])
+
+    for x in x:
+        f.writerow([x["description"],
+                    x["agency_responsible"]])
